3533071091371e64612d43506666c84ec95d0375
Derek Olson <dolson@prophet.com>
2013-09-15 11:05:48 -0500

    video hover state

--- js/main.js
+++ js/main.js
@@ -14,6 +14,12 @@ function init() {
         		videoWrapper.css('background-image', 'url('+video.thumbnail_large+')');
         		videoWrapper.append('<div class="play-button"></div>');
 
+                videoWrapper.hover(function() {
+                    $('.play-button', this).addClass('hover');
+                }, function() {
+                    $('.play-button', this).removeClass('hover');
+                });
+
         		videoWrapper.on('click', function(){
         			var id = $(this).data('vimeoid');
             		$(this).html('<iframe id="player'+id+'" src="//player.vimeo.com/video/'+id+'?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1&amp;api=1" width="960" frameborder="0" webkitAllowFullScreen mozallowfullscreen autoPlay allowFullScreen></iframe>');
