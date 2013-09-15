8fb8f0e42f4ee96ff9d7172e09e8b9bcc7aa5979
Derek Olson <dolson@prophet.com>
2013-09-15 15:20:12 -0500

    fixed header offset, vimeo overlays

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ function init() {
 
         		videoWrapper.on('click', function(){
         			var id = $(this).data('vimeoid');
-            		$(this).html('<iframe id="player'+id+'" src="//player.vimeo.com/video/'+id+'?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1&amp;api=1" width="960" frameborder="0" webkitAllowFullScreen mozallowfullscreen autoPlay allowFullScreen></iframe>');
+            		$(this).html('<iframe id="player'+id+'" src="//player.vimeo.com/video/'+id+'?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1&amp;api=1" frameborder="0" webkitAllowFullScreen mozallowfullscreen autoPlay allowFullScreen></iframe>');
             		
             		// var iframe = $('#player'+id)[0];
             		// var player = $f(iframe);
